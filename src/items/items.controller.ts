6ac2a18c92967d3458c743db667e9e7ca1ffccd7
youkaiantena <apple.kitaro@gmail.com>
2024-02-05 07:00:04 +0900

    feat: createメソッド追加

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ItemsService } from './items.service';
+import { Item } from './item.model';
+import { ItemStatus } from './item-status.enum';
 
 @Controller('items')
 export class ItemsController {
@@ -8,5 +10,23 @@ export class ItemsController {
   @Get()
   findAll() {
     return this.itemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    };
+
+    return this.itemsService.create(item);
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './item.model';
 
 @Injectable()
 export class ItemsService {
+  private items: Item[] = [];
+
   findAll() {
     return 'This is ItemsService';
+  }
+
+  // voidにすべき
+  create(item: Item): Item {
+    this.items.push(item);
+
+    return item;
   }
 }
