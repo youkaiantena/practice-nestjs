e592826e4cb911f6f67a1689e588c1bf86dbbecb
youkaiantena <apple.kitaro@gmail.com>
2024-02-05 23:03:45 +0900

    feat: Readメソッドをちょっと変更

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -5,8 +5,8 @@ import { Item } from './item.model';
 export class ItemsService {
   private items: Item[] = [];
 
-  findAll() {
-    return 'This is ItemsService';
+  findAll(): Item[] {
+    return this.items;
   }
 
   // voidにすべき
