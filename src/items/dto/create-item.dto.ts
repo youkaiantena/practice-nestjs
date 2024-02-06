6469a10a70fd08cb19551ac54993bcf30e43e791
youkaiantena <apple.kitaro@gmail.com>
2024-02-07 01:39:04 +0900

    feat: ClassValiator/ClassTransformer を追加
    - DomainModelとなるclassがバリデーションを持つためにはどうするか？？

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,6 +1,19 @@
+import { Type } from "class-transformer";
+import { IsInt, IsNotEmpty, IsString, MaxLength, Min } from "class-validator";
+
 // class validator を利用するためにclassで作成する
 export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(40)
   name: string;
+
+  @IsInt()
+  @Min(1)
+  @Type(() => Number)
   price: number;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
 }
